783fb345747f02191ef7892600a22e301e70e730
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-08-14 19:12:05 +0530

    UI completed

--- withBootStrap/Calculator/src/Components/ButtonContainer.jsx
+++ withBootStrap/Calculator/src/Components/ButtonContainer.jsx
@@ -1,12 +1,29 @@
 import styles from "./ButtonContainer.module.css";
 const ButtonContainer = () => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <div className={styles.btnCont}>
-      <button className={styles.btn}>C</button>
-      <button className={styles.btn}>C</button>
-      <button className={styles.btn}>C</button>
-      <button className={styles.btn}>C</button>
-      <button className={styles.btn}>C</button>
+      {buttonNames.map((buttonName) => (
+        <button className={styles.btn}>{buttonName}</button>
+      ))}
     </div>
   );
 };
