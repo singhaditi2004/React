7a662db594a70b02a9e19d97b779ec2dac4f9d9f
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-09-19 13:28:39 +0530

    use effect hook added

--- withBootStrap/latest/src/Components/CurrentTime.jsx
+++ withBootStrap/latest/src/Components/CurrentTime.jsx
@@ -1,6 +1,13 @@
-const CurrentTime = () => {
-  let time = new Date();
+import { useEffect, useState } from "react";
 
+const CurrentTime = () => {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
   return <>{time.toLocaleTimeString()}</>;
 };
 
