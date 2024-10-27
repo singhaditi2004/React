56c8463927e9cd879ade2c23fd3590dccc16ca21
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-10-27 13:27:52 +0530

    improved

--- withBootStrap/ReduxLearn/LearnRedux.js
+++ withBootStrap/ReduxLearn/LearnRedux.js
@@ -1,14 +1,18 @@
-const redux= require('redux');
-const INITIAL_VAL={
-    counter:0
+const redux = require("redux");
+const INITIAL_VAL = {
+  counter: 0,
 };
-const reducer=(store=INITIAL_VAL,action)=>{
-    return {counter: store.counter+1};
-}
-const createStore= redux.createStore(reducer); 
-const subscriber =()=>{
-    const state=createStore.getState();
-    console.log(state);
-}
+const reducer = (store = INITIAL_VAL, action) => {
+  let newStore = store;
+  if (action.type === "INCREMENT") {
+    newStore = { counter: store.counter + 1 };
+  }
+    return newStore;
+};
+const createStore = redux.createStore(reducer);
+const subscriber = () => {
+  const state = createStore.getState();
+  console.log(state);
+};
 createStore.subscribe(subscriber);
-createStore.dispatch({type:'INCREMENT' });
+createStore.dispatch({ type: "INCREMENT" });
