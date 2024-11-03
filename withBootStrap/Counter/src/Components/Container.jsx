c452eb2d845b4b42b700f5530490865a59403019
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-11-03 21:34:07 +0530

    added

--- withBootStrap/Counter/src/Components/Container.jsx
+++ withBootStrap/Counter/src/Components/Container.jsx
@@ -1,7 +1,7 @@
 const Modal = ({ children }) => {
   return (
     <>
-      <div className="card" style={{ width: "75%" }}>
+      <div className="card" style={{ width: "70%" }}>
         <div className="card-body">{children}</div>
       </div>
     </>
