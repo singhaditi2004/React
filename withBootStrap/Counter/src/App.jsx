3ac090f11e7d667268430b5b3b3e3e353cff4028
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-11-04 14:26:10 +0530

    added component

--- withBootStrap/Counter/src/App.jsx
+++ withBootStrap/Counter/src/App.jsx
@@ -3,36 +3,22 @@ import DisplayValue from "./Components/DisplayValue";
 import Header from "./Components/Header";
 import Modal from "./Components/Container";
 import "bootstrap/dist/css/bootstrap.css";
+import Controls from "./Components/Controls";
 
 function App() {
   return (
-    <>
-      <center
-        className="px-4 py-5 my-5 
+    <center
+      className="px-4 py-5 my-5 
       text-center"
-      >
-        <Modal>
-          <div className="col-lg-6 mx-auto">
-            <Header />
-            <DisplayValue />
-            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg px-4 gap-3"
-              >
-                Primary button
-              </button>
-              <button
-                type="button"
-                className="btn btn-outline-secondary btn-lg px-4"
-              >
-                Secondary
-              </button>
-            </div>
-          </div>
-        </Modal>
-      </center>
-    </>
+    >
+      <Modal>
+        <div className="col-lg-6 mx-auto">
+          <Header />
+          <DisplayValue />
+          <Controls />
+        </div>
+      </Modal>
+    </center>
   );
 }
 
