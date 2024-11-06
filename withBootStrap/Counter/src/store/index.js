8742cef2bcebd6a892fb5fbc0bf0ed3dc7ea6608
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-11-06 15:39:57 +0530

    stareted working

--- withBootStrap/Counter/src/store/index.js
+++ withBootStrap/Counter/src/store/index.js
@@ -3,6 +3,13 @@ import { createStore } from "redux";
 const INITIAL_VALUE = { counter: 0 };
 
 const counterReducer = (store = INITIAL_VALUE, action) => {
+  console.log("Action Recieved", action);
+  if (action.type === "INCREMENT") {
+    return { counter: store.counter + 1 };
+  }
+  if (action.type === "DECEREMENT") {
+    return { counter: store.counter - 1 };
+  }
   return store;
 };
 const counterStore = createStore(counterReducer);
