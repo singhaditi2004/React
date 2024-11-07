5117b7de89b1ce7f401fb8c0650252698dc6d003
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-11-07 10:05:02 +0530

    added files

--- withBootStrap/Counter/src/Components/Controls.jsx
+++ withBootStrap/Counter/src/Components/Controls.jsx
@@ -1,12 +1,33 @@
 import { useDispatch } from "react-redux";
+import "../App.css";
+import { useRef } from "react";
 
 const Controls = () => {
   const dispatch = useDispatch();
+  const inputEle = useRef();
   const handleIncrement = () => {
     dispatch({ type: "INCREMENT" });
   };
   const handleDecrement = () => {
     dispatch({ type: "DECEREMENT" });
+  };
+  const handleAdd = () => {
+    dispatch({
+      type: "ADD",
+      payload: {
+        value: inputEle.current.value,
+      },
+    });
+    inputEle.current.value = "";
+  };
+  const handleSubtract = () => {
+    dispatch({
+      type: "SUBTRACT",
+      payload: {
+        value: inputEle.current.value,
+      },
+    });
+    inputEle.current.value = "";
   };
   return (
     <>
@@ -24,6 +45,28 @@ const Controls = () => {
           className="btn btn-outline-secondary btn-lg px-4"
         >
           -1
+        </button>
+      </div>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+        <input
+          type="text"
+          className="btn btn-primary btn-lg px-4 gap-3 number-input "
+          placeholder="Enter number"
+          ref={inputEle}
+        />
+        <button
+          type="button"
+          className="btn btn-info btn-lg px-4"
+          onClick={handleAdd}
+        >
+          Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger btn-lg px-4"
+          onClick={handleSubtract}
+        >
+          Subtract
         </button>
       </div>
     </>
