ed9879b971396851196dd1a56eea43b4c3084801
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-12-29 23:03:55 +0530

    added

--- withBootStrap/Myntraclone/3-MyntraClone/src/Components/FetchItem.jsx
+++ withBootStrap/Myntraclone/3-MyntraClone/src/Components/FetchItem.jsx
@@ -1,6 +1,17 @@
-import { useSelector } from "react-redux";
+import { useSelector, useEffect } from "react-redux";
 const FetchItems = () => {
   const fetchStatus = useSelector((store) => store.fetchStatus);
+  useEffect(() => {
+    setFetching(true);
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addPosts(data.posts);
+        setFetching(false);
+      });
+      
+  }, []);
+
   return (
     <div>
       FetchDone: {fetchStatus.fetchDone}
